ad13739da103394feca0348a383c4f83d7148573
Victor Stanley <victorstanley2222@gmail.com>
2024-02-15 18:09:31 +0100

    Creating NavBar component

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,19 @@
 import { Grid, GridItem, Show } from "@chakra-ui/react"
+import NavBar from "./components/NavBar"
 
 
 function App() {
-  return <Grid templateAreas={{
+  return  <Grid templateAreas={{
+    
     base: `"nav" "main"`,
     lg: `"nav nav" "aside main"`
   }}>
-
-          <GridItem area='nav' bg='red'>nav</GridItem>
+          <GridItem area='nav'><NavBar /></GridItem>
           <Show above="lg">
             <GridItem area='aside' bg="yellow">aside</GridItem>
           </Show>
           <GridItem area='main' bg= 'blue'>main</GridItem>
+          
   </Grid>
 }
 
